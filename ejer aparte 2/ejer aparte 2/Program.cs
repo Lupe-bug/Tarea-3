b9c007b3c84d09e04fc2d33346561253f8b520d4
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:57:44 -0600

    Add files via upload

---
+++ ejer aparte 2/ejer aparte 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ejer_aparte_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, numero;
+            factorial largo=1;
+            Consola.Escribir(" Ingrese un número:");
+            numero=int.Analizar(Consola.ReadLine());
+            Consola.WriteLine("\ n"+numero +"!=1");
+            para(i=2;i<=numero;i++);
+            {
+                factorial*=i;
+                Consola.WriteLine("\u00D7"+ i);
+            }
+            Consola.WriteLine("="+factorial);
+            Consola.WriteLine("Presione cualquier tecla para terminar...");
+            Consola.ReadKey();
+        }
+    }
+}
