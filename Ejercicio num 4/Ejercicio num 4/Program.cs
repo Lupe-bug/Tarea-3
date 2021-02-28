410c9067bb6a5c61fbcf35303d9e3d90649e9e77
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:34:35 -0600

    Add files via upload

---
+++ Ejercicio num 4/Ejercicio num 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ejercicio_num_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Ingrese un numero del 1 al 7: ");
+            num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Lunes");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Martes");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Miercoles");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Jueves");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Viernes");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Sabado");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Domingo");
+                        break;
+                    }
+            }
+        }
+    }
+}
