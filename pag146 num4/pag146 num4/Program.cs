737fd5f7325869568376b02bdc5bb9908838e816
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:50:09 -0600

    Add files via upload

---
+++ pag146 num4/pag146 num4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pag146_num4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2, i, contador = 0;
+            while (num <= 1000)
+            {
+                for (i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        contador++;
+                    }
+                }
+                if (contador == 2)
+                {
+                    Console.WriteLine("El siguiente numero es primo: " + num);
+                }
+                contador = 0;
+                num++;
+            }
+        }
+    }
+}
