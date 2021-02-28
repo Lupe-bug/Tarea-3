768fbc571974bcf0cf7f3de555f84c0cf0daeb0c
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:46:31 -0600

    Add files via upload

---
+++ pag146 num2/pag146 num2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pag146_num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Base, Exponente;
+
+            Consola.WriteLine("Ingresa la base de la operacion:");
+            Base = Convertir.ToInt32(Console.ReadLine());
+
+            Consola.Borrar();
+
+            Consola.WriteLine("Ahora ingrese el exponente a elevarlo.");
+            Exponente = Convertir.ToInt32(Console.ReadLine());
+
+            Consola.Borrar();
+
+            Consola.WriteLine( $ " Base: { Base }, Exponente { Exponente }. -> { Base } ^ { Exponente } \ n Resultado: { Math . Pow ( Base , Exponente )} ");
+
+            Consola.ReadKey();
+        }
+    }
+}
