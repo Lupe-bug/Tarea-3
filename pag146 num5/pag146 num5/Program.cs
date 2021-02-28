9c4ff50b02bbb6c8089e39c37e2cb5cd4bad58e3
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:52:13 -0600

    Add files via upload

---
+++ pag146 num5/pag146 num5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pag146_num5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numper, i, mayor = 0, menor = 0;
+            Console.Write("Cuantas Edades desea ingresar: ");
+            numper = Convert.ToInt32(Console.ReadLine());
+            int[] edades = new int[numper + 1];
+            for (i = 1; i <= numper; i++)
+            {
+                Console.Write("Cual es la Edad de la Persona: ");
+                edades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            mayor = edades[1];
+            menor = edades[1];
+            for (i = 1; i <= numper; i++)
+            {
+                if (edades[i] > mayor)
+                {
+                    mayor = edades[i];
+                }
+                else if (edades[i] < menor)
+                {
+                    menor = edades[i];
+                }
+            }
+
+            Console.WriteLine("La edad mayor es: " + mayor);
+            Console.WriteLine("La edad menor es: " + menor);
+        } 
+    }
+    
+}
