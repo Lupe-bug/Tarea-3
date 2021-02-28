e6f29e0d125534fc8f96c54d790bc9c932e6c1a4
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:36:20 -0600

    Add files via upload

---
+++ ejercicio num5/ejercicio num5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ejercicio_num5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            double resultado, num2, longi, apotema;
+            Console.WriteLine("Que desea hacer");
+            Console.WriteLine("1 - calcular el perimetro");
+            Console.WriteLine("2 - calcular el area de cualquier poligono regular");
+            Console.Write("............       ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 1)
+            {
+                Console.Write("Introduzca el diametro: ");
+                num2 = double.Parse(Console.ReadLine());
+                resultado = (num2 * 3.14159);
+                Console.WriteLine("El resultado es de: " + resultado);
+            }
+            else if (num == 2)
+            {
+                Console.Write("Introduzca la longitud: ");
+                longi = double.Parse(Console.ReadLine());
+                Console.Write("Introduzca la longitud: ");
+                apotema = double.Parse(Console.ReadLine());
+                resultado = (5 * longi * apotema);
+                Console.WriteLine("El resultado es de: " + resultado);
+
+
+            }
+        }
+    }
+}
