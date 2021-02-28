dc7d9656509bbee640984048602d85294e19b413
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:31:32 -0600

    Add files via upload

---
+++ ejercicio num3/ejercicio num3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ejercicio_num3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2;
+            float resultado;
+            Console.WriteLine("1 - Medicamentos");
+            Console.WriteLine("2 - otros");
+            Console.Write("............    ");
+            num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    {
+                        Console.Write("Ingrese la cantidad del producto:");
+                        num2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El impuesto aplicado es de 0% y el total es de: " + num2);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("Ingrese la cantidad del producto:");
+                        num2 = int.Parse(Console.ReadLine());
+                        resultado = (float)(num2 * 0.05);
+                        Console.WriteLine("El impuesto aplicado es de 15% y el total es de: " + resultado);
+                        break;
+                    }
+            }
+
+        }   }
+}
