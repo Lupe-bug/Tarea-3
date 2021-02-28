8502b4515c4db9879c74a56352b10932db7453cf
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:29:53 -0600

    Add files via upload

---
+++ Ejercicio num2/Ejercicio num2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ejercicio_num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultado, num, num2;
+            Console.WriteLine("1 - De grados a radianes");
+            Console.WriteLine("2 - De radianes a grados");
+            Console.Write("............    ");
+            num = float.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    {
+
+                        Console.Write("Ingresa la cantidad a convertir: ");
+                        num2 = float.Parse(Console.ReadLine());
+                        resultado = num2 * 3.1416 / 180;
+                        Console.WriteLine("El resultado es: " + resultado);
+                        break;
+                    }
+                case 2:
+                    {
+
+                        Console.Write("Ingresa la cantidad a convertir: ");
+                        num2 = float.Parse(Console.ReadLine());
+                        resultado = num2 * 180 / 3.1416;
+                        Console.WriteLine("El resultado es: " + resultado);
+                        break;
+                    }
+            }
+
+        }
+    }
+}
