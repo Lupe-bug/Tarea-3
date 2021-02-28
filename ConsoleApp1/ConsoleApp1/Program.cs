2a86de1b71782b0c9fb0ad293a4ab7c43469c32b
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 21:57:37 -0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.Write("Digite un numero para saber si es numero primo o impar: ");
+            num1 = int.Parse(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("El numero ingresado es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es impar");
+            }
+        }
+    }
+}
