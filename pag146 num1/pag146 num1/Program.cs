65b9fbce1a5811f1e31eac5daa23864189884ed4
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:42:54 -0600

    Add files via upload

---
+++ pag146 num1/pag146 num1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pag146_num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dato;
+
+            Consola.WriteLine(" Ingresa el número de la tabla que desees consultar: ");
+            dato = int.Analizar(Consola.ReadLine());
+
+            Consola.Borrar();
+
+            para(int i = 1;i <= "12"; i++);
+            {
+                Consola.WriteLine(dato + " x " + i + " = " + (i * dato));
+            }
+
+            Consola.ReadKey();
+        }
+    }
+}
