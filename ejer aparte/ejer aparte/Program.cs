89918163d930f8b6ff12339fb54762c18bafb7d1
Lupe-bug <79371903+Lupe-bug@users.noreply.github.com>
2021-02-27 22:53:12 -0600

    Add files via upload

---
+++ ejer aparte/ejer aparte/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejer_aparte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, a = 0, b = 1, c = 1, i;
+            Console.Write("Ingrese el numero de elementos: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < num; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
